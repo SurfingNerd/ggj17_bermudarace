ae9cd6861f9c2a31192fb67f8c56b9a042a7e86a
Thomas Haller <ThomasHaller@gmx.at>
2017-01-21 12:20:34 +0100

    Architecture  treasures

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    //HashSet<Treasure>
+
+    [HideInInspector]
+    public PlayerControl control;
+
+	// Use this for initialization
+	void Start () {
+        control = GetComponent<PlayerControl>(); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
