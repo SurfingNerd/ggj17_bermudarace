0b843cbcc419bb86b52dd71103197a09669e0d10
Christian Afonso <chr.afonso@gmail.com>
2017-01-21 13:52:39 +0100

    Added Titlesceen script and background, click/key to load next scene

---
+++ Assets/Scripts/TitleSceen/TitleScreen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour {
+
+	public string NextScene = "test_scene";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.anyKeyDown || Input.GetMouseButtonDown(0))
+		{
+			SceneManager.LoadScene(NextScene);
+		}
+	}
+}
