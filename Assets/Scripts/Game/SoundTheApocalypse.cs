60769274c90d5e5e44445521d64ac0f3e4c91e7e
Christian Afonso <chr.afonso@gmail.com>
2017-01-22 04:28:51 +0100

    Added music

---
+++ Assets/Scripts/Game/SoundTheApocalypse.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTheApocalypse : MonoBehaviour {
+
+	public AudioClip MusicOfTheApocalypse;
+
+	public void SoundTheApocalypseNow()
+	{
+		AudioSource source = Camera.main.GetComponent<AudioSource>();
+		source.Stop();
+		source.clip = MusicOfTheApocalypse;
+		source.loop = true;
+		source.Play();
+	}
+
+}
